60470454ae8a39d7d57ef8af7af8248aec0e57f5
Mark Howe <mvahowe@gmail.com>
2021-08-18 14:42:26 +0200

    Tokenize strings using lexingRegexes

--- src/Pages/MyPages/edit/save_aghast.js
+++ src/Pages/MyPages/edit/save_aghast.js
@@ -1,4 +1,13 @@
+import xre from 'xregexp';
 import { lexingRegexes } from 'proskomma';
+
+const mainRegex = xre.union(lexingRegexes.map((x) => x[2]));
+const tokenTypes = {};
+for (const lr of lexingRegexes) {
+    if (['eol', 'lineSpace', 'punctuation', 'wordLike'].includes(lr[1])) {
+        tokenTypes[lr[1]] = lr[2];
+    }
+}
 
 const saveAghast = rawAghast => {
 
@@ -6,11 +15,29 @@ const saveAghast = rawAghast => {
         return a.map(b => ({...b, children: b.children.filter(c => c.type || (c.text && c.text !== ''))}));
     }
 
+    const tokenizeString = str => {
+        const ret = [];
+        for (const token of xre.match(str, mainRegex, 'all')) {
+            let tokenType;
+            if (xre.test(token, tokenTypes['wordLike'])) {
+                tokenType = 'wordLike';
+            } else if (xre.test(token, tokenTypes['punctuation'])) {
+                tokenType = 'punctuation';
+            } else if (xre.test(token, tokenTypes['lineSpace']) || xre.test(token, tokenTypes['eol'])) {
+                tokenType = 'lineSpace';
+            } else {
+                tokenType = 'unknown';
+            }
+            ret.push([token, tokenType]);
+        }
+        return ret;
+    }
     const processItems = items => {
         const ret = [];
         for (const item of items) {
             if (!item.type && item.text) {
-                for (const [tokenText, tokenType] of [[item.text, 'wordLike']]) {
+                const tokenized = tokenizeString(item.text);
+                for (const [tokenText, tokenType] of tokenized) {
                     ret.push({
                         type: 'token',
                         subType: tokenType,
